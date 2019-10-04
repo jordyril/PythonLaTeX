acac5564810ebac83a3dd80e6a6c747b60ae6d24
jordy_bf.uzh <jordy.rillaerts@bf.uzh.ch>
2019-10-04 10:42:05 +0200

    table class + testing

--- build/lib/pythonlatex/table.py
+++ build/lib/pythonlatex/table.py
@@ -5,38 +5,42 @@ This module modifies the 'Table' class from 'pylatex'
 """
 
 from pylatex import Table as TableOriginal
-from pylatex import Package, NoEscape, Tabular
+from pylatex import Tabular as TabularOriginal
+from pylatex import Package, NoEscape, UnsafeCommand
+from pylatex.utils import fix_filename, escape_latex
 from .saving import LatexSaving
+from .float import FloatAdditions
 import pandas as pd
 
 
-class Table(LatexSaving, TableOriginal):
+class Table(FloatAdditions, LatexSaving, TableOriginal):
     """A class that represents a Table environment with modified methods compared to parent"""
 
     def __init__(
         self,
         *args,
-        folder_path="Latex/",
-        folder_name="Tables",
-        create_folder=True,
+        folders_path="Latex/",
+        outer_folder_name="Tables",
+        inner_folder_name="Tabulars",
         position=None,
         **kwargs,
     ):
+
         LatexSaving.__init__(
             self,
-            folder_name=folder_name,
-            folder_path=folder_path,
-            create_folder=create_folder,
+            outer_folder=outer_folder_name,
+            inner_folder=inner_folder_name,
+            folders_path=folders_path,
         )
-        TableOriginal.__init__(self, *args, position=position, **kwargs)
 
+        TableOriginal.__init__(self, *args, position=position, **kwargs)
         self._label = "tbl"
 
     def _set_tabular(self, tabular, *args, **kwargs):
         """
         TODO
         """
-        if isinstance(tabular, Tabular):
+        if isinstance(tabular, TabularOriginal):
             self.tabular = tabular.dumps()
 
         elif isinstance(tabular, str):
@@ -45,38 +49,135 @@ class Table(LatexSaving, TableOriginal):
         elif isinstance(tabular, pd.DataFrame):
             self.tabular = tabular.to_latex(*args, **kwargs)
 
-    def _tabular_from_df(self, df):
-        """
-        TODO
-        """
-
-        nbr_row, nbr_col = df.shape
-
-        # both have to be increased by 1 for headerrow and index column
-        nbr_col += 1
-        nbr_row += 1
-
-    def _create_tabular_file(self, filename):
+    def _save_tabular(self, filename):
         try:
             self.tabular
         except AttributeError:
             raise AttributeError("No tabular set to save")
 
-        with open(self._absolute_path(f"{filename}.tex"), "w+") as file:
+        with open(self._absolute_inner_path(f"{filename}.tex"), "w+") as file:
             file.write(self.tabular)
 
-    # def input_from_to_latex_string(
-    #     self,
-    #     to_latex_string,
-    #     name,
-    #     caption,
-    #     label=None,
-    #     above=True,
-    #     **latex_input_kwargs,
-    # ):
-    #     # write to_latex_tabular to .tex file - create tabular file
-    #     with open(self._absolute_path(name + ".tex"), "w") as file:
-    #         file.write(to_latex_string)
-    #     # self.create_latex_input(name, caption, label, above, **latex_input_kwargs)
+        return self._relative_inner_path(filename)
 
-    #     return None
+    def add_table(
+        self,
+        tabular,
+        filename,
+        *args,
+        caption=None,
+        above=True,
+        label=None,
+        placement=NoEscape(r"\centering"),
+        **kwargs,
+    ):
+        """Add an image to the figure.
+        Args
+        ----
+        filename: str
+            Filename of the image.
+        placement: str
+            Placement of the table, `None` is also accepted.
+        """
+        if label is None:
+            label = filename
+
+        self._set_tabular(tabular)
+        path = self._save_tabular(filename)
+
+        if placement is not None:
+            self.append(placement)
+
+        self.append(StandAloneTabular(filename=fix_filename(path)))
+
+        if caption is not None:
+            self.add_caption_label(caption, label, above)
+
+    def reset(self):
+        self.data = []
+        del self.tabular
+
+    def create_input_latex(
+        self,
+        tabular,
+        filename,
+        *args,
+        add_table=True,
+        caption=None,
+        above=True,
+        label=None,
+        **kwargs,
+    ):
+        """Creates separate input tex-file that can be used to input tabular within table environment
+        Args
+        ----
+        filename: str
+            Name of the table for saving
+        tabular: str, pandas.DataFrame, Tabular
+            tabular that will be saved and created into a proper table
+        args:
+            Arguments passed to pd.df.to_latex for displaying the tabular.
+        add_table: bool
+            In case of normal table this is True, in case of subtable, no new table needs
+            to be added so this option should be set to False
+        caption: str
+            Optional caption to be added to the table
+        above: bool
+            In case caption is given, position of caption can be above or bellow table
+        extension : str
+            Extension of image file indicating table file type
+        kwargs:
+            Keyword arguments passed to plt.savefig for displaying the plot.
+        """
+        # TODO
+        if add_table:
+            self.add_table(
+                tabular,
+                filename,
+                *args,
+                caption=caption,
+                above=above,
+                label=label,
+                **kwargs,
+            )
+        else:
+            self.add_caption_label(caption, label, above)
+
+        # creating + opening the file
+        with open(self._absolute_outer_path(f"{filename}.tex"), "w") as tex_file:
+            tex_file.write(self.dumps())
+
+        latex_input = self._print_latex_input(filename)
+        self._write_input_to_txt_file(latex_input)
+
+        return NoEscape(latex_input)
+
+
+class SubTable(Table):
+    """
+    """
+
+    def __init__(self, width=NoEscape(r"0.49\linewidth"), **kwargs):
+        super().__init__(arguments=width, **kwargs)
+
+
+class StandAloneTabular(UnsafeCommand):
+    r"""A class representing a stand alone tabular. (\input{tabularfile})"""
+
+    _latex_name = "input"
+
+    _repr_attributes_mapping = {"filename": "arguments"}
+
+    def __init__(self, filename):
+        r"""
+        Args
+        ----
+        filename: str
+            The path to the tabular file
+        image_options: str or `list`
+            Specifies the options for the image (ie. height, width)
+        """
+
+        arguments = [NoEscape(filename)]
+
+        super().__init__(command=self._latex_name, arguments=arguments)
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,11 +1,13 @@
-# import numpy as np
 from pythonlatex import Table
+from pylatex import Document, NoEscape
+import pandas as pd
+import numpy as np
 
 # from pylatex import Document, NoEscape
 
 import unittest
 
-# import os
+import os
 import shutil
 
 try:
@@ -23,57 +25,97 @@ df.index.name = "index"
 
 class TestTables(unittest.TestCase):
     def test_path(self):
-        path = "./test"
-        table = Table(folder_path=path, create_folder=False)
-        self.assertEqual(table._folder_path, path)
+        path = "./Latex/test_path/"
+        table = Table(folders_path=path)
+        self.assertEqual(table._folders_path, path)
 
-    def test_folder(self):
-        folder = "test_folder"
-        table = Table(folder_name=folder, create_folder=False)
+    def test_inner_folder(self):
+        folder = "Inner_test"
+        table = Table(inner_folder_name=folder)
 
-        self.assertEqual(table._folder_name, folder)
+        self.assertEqual(table._inner_folder_name, folder)
+
+    def test_outer_folder(self):
+        folder = "Outer_test"
+        table = Table(outer_folder_name=folder)
+
+        self.assertEqual(table._outer_folder_name, folder)
 
     def test_position(self):
         position = "h"
-        table = Table(position=position, create_folder=False)
+        table = Table(position=position)
         self.assertEqual(table.options, position)
-    
-    def test_tabularfile(self):
-        
 
+    def test_set_tabular(self):
+        table = Table()
+        table._set_tabular(df)
 
-    # def test_reset(self):
-    #     table = Table()
-    #     # Table 1
-    #     for i in range(1, 3):
-    #         name = f"test{i}"
-    #         plt.Table()
-    #         plt.plot(x, y, label=i)
-    #         table.add_plot(name)
+    def test_save_tabular(self):
+        table = Table()
+        table._set_tabular(df)
+        name = "test"
+        table._save_tabular(name)
 
-    #         # check if correct latex output is produced
-    #         self.assertEqual(
-    #             table.dumps(),
-    #             (
-    #                 f"\\begin{{Table}}%\n\\centering%\n\\includegraphics[width=0.8\\textwidth]"
-    #                 + f"{{Tables/test{i}.jpg}}%\n\\end{{Table}}"
-    #             ),
-    #         )
-    #         table.reset(close=True)
+        # check if graph was saved
+        path = table._absolute_inner_path(f"{name}.tex")
+        self.assertTrue(os.path.isfile(path))
 
-    # def test_texinput(self):
-    #     table = Table()
-    #     name = "test_tex"
-    #     caption = "caption"
-    #     plt.Table()
-    #     plt.plot(x, y)
-    #     input_tex = table.write_input_latex(name, caption=caption, above=False)
+    def test_add_table(self):
+        table = Table()
+        table._set_tabular(df)
+        name = "test"
 
-    #     # create document for testing input statement
-    #     doc = Document()
-    #     doc.append(input_tex)
-    #     doc.preamble.append(NoEscape(r"\usepackage{graphicx}"))
-    #     doc.generate_pdf("Latex/test", clean_tex=False)
+        table.add_table(df, name)
+
+        self.assertEqual(
+            table.dumps(),
+            "\\begin{table}%\n\\centering%\n\\input{Tabulars/test}%\n\\end{table}",
+        )
+
+    def test_caption(self):
+        table = Table()
+        table._set_tabular(df)
+        name = "test"
+        caption = "caption"
+
+        table.add_table(df, name, caption=caption)
+
+        self.assertEqual(
+            table.dumps(),
+            (
+                "\\begin{table}%\n\\caption{caption}%\n\\zlabel{tbl:test}%\n\\"
+                "centering%\n\\input{Tabulars/test}%\n\\end{table}"
+            ),
+        )
+
+    def test_reset(self):
+        table = Table()
+        for i in range(1, 3):
+            name = f"test{i}"
+            table.add_table(df, name, caption=name)
+
+            # check if correct latex output is produced
+            self.assertEqual(
+                table.dumps(),
+                (
+                    f"\\begin{{table}}%\n\\caption{{{name}}}%\n\\zlabel{{tbl:{name}}}%\n\\"
+                    f"centering%\n\\input{{Tabulars/{name}}}%\n\\end{{table}}"
+                ),
+            )
+            table.reset()
+
+    def test_texinput(self):
+        table = Table()
+        name = "test_tex"
+        caption = "caption"
+        input_tex = table.create_input_latex(df, name, caption=caption, above=False)
+
+        # create document for testing input statement
+        doc = Document()
+        doc.append(input_tex)
+        doc.preamble.append(NoEscape(r"\usepackage{booktabs}"))
+        doc.preamble.append(NoEscape(r"\usepackage{zref-user}"))
+        doc.generate_pdf("Latex/test", clean_tex=False)
 
 
 if __name__ == "__main__":
